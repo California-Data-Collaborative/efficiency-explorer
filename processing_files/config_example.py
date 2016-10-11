d0e26f15b73be121774a59df7ce735d32e956b31
thenamesdave <dmmarulli@gmail.com>
2016-10-11 11:51:52 -0700

    added example config file

---
+++ processing_files/config_example.py
@@ -0,0 +1,12 @@
+# Extract and transform routine
+database = "<DATABASE_NAME>"
+user = "<USER_NAME>"
+password = "<USER_PASSWORD>"
+host = "<HOST_NAME>"
+port = "<PORT_NUMBER>"
+
+anonymityDict = {"<UTILITY_ID>" : "<UTILITY_RANDOM_PREFIX>"}
+
+
+# Load routine
+apikey = "<CARTO_API_KEY>"
