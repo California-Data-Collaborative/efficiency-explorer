c76946180b7b54207b59b7938bf41ee0d9097c0a
thenamesdave <dmmarulli@gmail.com>
2017-04-05 16:14:50 -0700

    full babel complie objects.js

--- js/objects.js
+++ js/objects.js
@@ -1,64 +1,59 @@
 ////// visualization state object and global data object
 var state = {
-	"placeID" : "<PLACE_IDENTIFIER>",
-	"hrName" : "<HUMAN_READABLE_IDENTIFIER>",
-	"startDate" : "<SCENARIO_START>",
-	"endDate" : "<SCENARIO_END>",
-	"gpcd" : "<GPCD>",
-	"pf" : "<PLANT_FACTOR>"
-}
+	"placeID": "<PLACE_IDENTIFIER>",
+	"hrName": "<HUMAN_READABLE_IDENTIFIER>",
+	"startDate": "<SCENARIO_START>",
+	"endDate": "<SCENARIO_END>",
+	"gpcd": "<GPCD>",
+	"pf": "<PLANT_FACTOR>"
+};
 
 var globals = {
-	"dateData" : "<DATE_DATA>",
-	"sublayers" : [],
-}
+	"dateData": "<DATE_DATA>",
+	"sublayers": []
+};
 
 //// set default state values and store global non-state-dependent data
 function dataSetup(callback) {
 	// choose random place for default placeID
 	var query = "\n\t\tSELECT DISTINCT " + config.column_names.unique_id + ", " + config.column_names.hr_name + " \n\t\tFROM " + config.attribute_table,
-
-		encoded_query = encodeURIComponent(query),
-		url = "https://" + config.account + ".carto.com/api/v2/sql?q=" + encoded_query;
+	    encoded_query = encodeURIComponent(query),
+	    url = "https://" + config.account + ".carto.com/api/v2/sql?q=" + encoded_query;
 	$.support.cors = true;
-	$.getJSON(url, function(idData) {
-		var	min = 0,
-			max = idData.total_rows,
-			randIdx =  Math.floor(Math.random() * (max - min)) + min,
-			randomPlace = idData.rows[randIdx][config.column_names.unique_id];
-			state.placeID = randomPlace
-			randomName = idData.rows[randIdx][config.column_names.hr_name];
-			state.hrName = randomName
-			transition("#hrName", randomName)
+	$.getJSON(url, function (idData) {
+		var min = 0,
+		    max = idData.total_rows,
+		    randIdx = Math.floor(Math.random() * (max - min)) + min,
+		    randomPlace = idData.rows[randIdx][config.column_names.unique_id];
+		state.placeID = randomPlace;
+		randomName = idData.rows[randIdx][config.column_names.hr_name];
+		state.hrName = randomName;
+		transition("#hrName", randomName);
 		// calculate most recent full^* month and 1 year back for default end and start dates, respectively
 		// ^*I exclude the actual most recent month because not every block contains these data
 		var query = "\n\t\t\tSELECT DISTINCT " + config.column_names.date + "\n\t\t\tFROM " + config.attribute_table + "\n\t\t\tORDER BY " + config.column_names.date + " DESC",
-
-			encoded_query = encodeURIComponent(query),
-			url = "https://" + config.account + ".carto.com/api/v2/sql?q=" + encoded_query;
-		$.getJSON(url, function(dateData) {
+		    encoded_query = encodeURIComponent(query),
+		    url = "https://" + config.account + ".carto.com/api/v2/sql?q=" + encoded_query;
+		$.getJSON(url, function (dateData) {
 			state.endDate = dateData.rows[1][config.column_names.date];
-			state.startDate = dateData.rows[12][config.column_names.date]; 
-			state.gpcd = 55
-			state.pf = .8
+			state.startDate = dateData.rows[12][config.column_names.date];
+			state.gpcd = 55;
+			state.pf = .8;
 
-			globals.dateData = dateData
-			callback()
+			globals.dateData = dateData;
+			callback();
 		});
 	});
 }
 
-
-
 ////// cartography object
 
 var cartography = {
-	"cartocss" : "<GEOM_DEPENDENT>",
-	"legend" : "<GEOM_DEPENDENT>",
-	'tooltip' :
-	"\n\t<div class=\"cartodb-tooltip-content-wrapper light\">\n\t<div class=\"cartodb-tooltip-content\">\n\t<h4>Place</h4>\n\t<p>{{hr_name}}</p>\n\t<h4>Percent Over/Under Target</h4>\n\t<p>{{percentdifference}}%</p>\n\t<h4>Population</h4>\n\t<p>{{population}}</p>\n\t</div>\n\t</div>\n\t"
+	"cartocss": "<GEOM_DEPENDENT>",
+	"legend": "<GEOM_DEPENDENT>",
+	'tooltip': "\n\t<div class=\"cartodb-tooltip-content-wrapper light\">\n\t<div class=\"cartodb-tooltip-content\">\n\t<h4>Place</h4>\n\t<p>{{hr_name}}</p>\n\t<h4>Percent Over/Under Target</h4>\n\t<p>{{percentdifference}}%</p>\n\t<h4>Population</h4>\n\t<p>{{population}}</p>\n\t</div>\n\t</div>\n\t"
 
-}
+};
 
 // define legend
 // (should probably be contained within some cartographySetup() function for increased elegance)
@@ -67,53 +62,44 @@ choropleth = new cdb.geo.ui.Legend({
 	show_title: true,
 	title: "Percent Over Target",
 	data: [{
-			value: "------------ 0% -------- 16% ------- 33% ------- 50% ----------"
-		}, {
-			value: ""
-		}, {
-			name: "bin1",
-			value: "#3EAB45"
-		}, {
-			name: "bin2",
-			value: "#B9D14C"
-		}, {
-			name: "bin3",
-			value: "#D9C24F"
-		}, {
-			name: "bin4",
-			value: "#D99F4F"
-		},		{
-			name: "bin5",
-			value: "#D9534F"
-		}]
-	})
+		value: "------------ 0% -------- 16% ------- 33% ------- 50% ----------"
+	}, {
+		value: ""
+	}, {
+		name: "bin1",
+		value: "#3EAB45"
+	}, {
+		name: "bin2",
+		value: "#B9D14C"
+	}, {
+		name: "bin3",
+		value: "#D9C24F"
+	}, {
+		name: "bin4",
+		value: "#D99F4F"
+	}, {
+		name: "bin5",
+		value: "#D9534F"
+	}]
+});
 
 if (config.geom_type == "point") {
 	var bubble = new cdb.geo.ui.Legend({
 		type: "bubble",
 		show_title: true,
 		title: "District Population",
-		data: [
-			{ value: "Smallest" },
-			{ value: "Largest" },
-			{ name: "graph_color", value: "#ccc" }
-			]
-		})
+		data: [{ value: "Smallest" }, { value: "Largest" }, { name: "graph_color", value: "#ccc" }]
+	});
 	cartography.legend = new cdb.geo.ui.StackedLegend({
 		legends: [bubble, choropleth]
-	})
-}
-
-else if (config.geom_type == "polygon") {
-		cartography.legend = choropleth
+	});
+} else if (config.geom_type == "polygon") {
+	cartography.legend = choropleth;
 };
 
 // define cartocss
 if (config.geom_type == "point") {
-	cartography.cartocss =
-	"#table {\n\t\tmarker-fill-opacity: .75;\n\t\tmarker-line-width: 0;\n\t\tmarker-width: 10;\n\t\tmarker-allow-overlap: true;\n\t\tpolygon-comp-op: multiply;\n\n\t\tmarker-width: ramp([population], range(5, 30), jenks(10));\n\n\t\tmarker-fill: #333;\n\t}\n\n\t\t#table [ percentdifference <= 0 ] {marker-fill: #3EAB45}\n\t\t#table [ percentdifference > 0 ] {marker-fill: #B9D14C}\n\t\t#table [ percentdifference > 16 ] {marker-fill: #D9C24F}\n\t\t#table [ percentdifference > 33 ] {marker-fill: #D99F4F}\n\t\t#table [ percentdifference > 50 ] {marker-fill: #D9534F}\n\t\t"
-	}
-else if (config.geom_type == "polygon") {
-		cartography.cartocss =
-		"#table {\n\t\t\tpolygon-fill: #333;\n\t\t\tpolygon-opacity: 0.6;\n\t\t\tline-width: 0.2;\n\t\t\tline-color: #222;\n\t\t\tline-opacity: 0.8;\n\t\t}\n\n\t\t#table [ percentdifference <= 0] {polygon-fill: #3EAB45;}\n\t\t#table [ percentdifference > 0] {polygon-fill: #B9D14C;}\n\t\t#table [ percentdifference > 16] {polygon-fill: #D9C24F;}\n\t\t#table [ percentdifference > 33] {polygon-fill: #D99F4F;}\n\t\t#table [ percentdifference > 50] {polygon-fill: #D9534F;}\n\t\t"
-	};
+	cartography.cartocss = "#table {\n\t\tmarker-fill-opacity: .75;\n\t\tmarker-line-width: 0;\n\t\tmarker-width: 10;\n\t\tmarker-allow-overlap: true;\n\t\tpolygon-comp-op: multiply;\n\n\t\tmarker-width: ramp([population], range(5, 30), jenks(10));\n\n\t\tmarker-fill: #333;\n\t}\n\n\t\t#table [ percentdifference <= 0 ] {marker-fill: #3EAB45}\n\t\t#table [ percentdifference > 0 ] {marker-fill: #B9D14C}\n\t\t#table [ percentdifference > 16 ] {marker-fill: #D9C24F}\n\t\t#table [ percentdifference > 33 ] {marker-fill: #D99F4F}\n\t\t#table [ percentdifference > 50 ] {marker-fill: #D9534F}\n\t\t";
+} else if (config.geom_type == "polygon") {
+	cartography.cartocss = "#table {\n\t\t\tpolygon-fill: #333;\n\t\t\tpolygon-opacity: 0.6;\n\t\t\tline-width: 0.2;\n\t\t\tline-color: #222;\n\t\t\tline-opacity: 0.8;\n\t\t}\n\n\t\t#table [ percentdifference <= 0] {polygon-fill: #3EAB45;}\n\t\t#table [ percentdifference > 0] {polygon-fill: #B9D14C;}\n\t\t#table [ percentdifference > 16] {polygon-fill: #D9C24F;}\n\t\t#table [ percentdifference > 33] {polygon-fill: #D99F4F;}\n\t\t#table [ percentdifference > 50] {polygon-fill: #D9534F;}\n\t\t";
+};
