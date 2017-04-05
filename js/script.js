0fcb897ed37654b250f51b047640d10b6ec89d15
thenamesdave <dmmarulli@gmail.com>
2017-04-05 16:59:00 -0700

    secured basemap tiles

--- js/script.js
+++ js/script.js
@@ -198,7 +198,7 @@ function mapSetup_dm() {
 	};
 
 	// Pull tiles from OpenStreetMap
-	L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
 		attribution: 'Powered by <a href="http://www.argolabs.org/">ARGO</a> | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>'
 	}).addTo(map);
 
